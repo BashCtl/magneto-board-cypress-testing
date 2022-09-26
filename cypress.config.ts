88620435e77f542bcead1215a7780e35aaa87b91
space <ki14.skakun@gmail.com>
2022-09-26 16:15:23 +0300

    update ci configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "nx6aoh",
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'

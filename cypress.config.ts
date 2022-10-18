89cb569688d27b461115582b63b78be1f7f77b61
space <ki14.skakun@gmail.com>
2022-10-18 12:02:58 +0300

    Update default comand timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   projectId: "nx6aoh",
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    defaultCommandTimeout: 10000
   }
 })
